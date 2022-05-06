34ca9493dcd81239e600c777d25a2a61541f3a4b
Ziray-a <samyprecupas@gmail.com>
2022-05-06 15:12:29 +0200

    API Addition & start dyn prototyp

---
+++ JS/Speisekarte.js
@@ -0,0 +1,11 @@
+
+/*
+TODO: 
+ELEMENT FÜR ESSEN definieren
+verbindung mit server herstellen
+get request senden
+!! WICHTIG !!
+mögliche redundanz ..> Speisekarte & Menu für editieren, vielleicht mit mehreren übergeordneten
+JS arbeiten und auf menu/speisekarte mit parametern unterscheiden
+GET für Speieskarte
+*/
---
+++ JS/hinzu.js
@@ -0,0 +1,28 @@
+let InfoJSON ={
+    name: undefined,
+    beschreibung: undefined,
+    zutaten: undefined,
+    rezept: undefined,
+    bild: undefined
+}
+/*
+TODO:
+POST für hinzufügen
+Löschvorgang?
+
+*/
+document.onload(initiate())
+
+
+function initiate(){
+let table = document.getElementById("tb");
+let submitbtn =document.getElementById("buttonsave");
+
+submitbtn.addEventListener("onclick", (clickevent) =>{
+clickevent.preventDefault();
+
+});
+
+    
+}
+
