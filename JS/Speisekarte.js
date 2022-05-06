39b53eb4e46c620809424fe0e4ac5b6a5ad2e5b9
Ziray-a <samyprecupas@gmail.com>
2022-05-06 16:28:24 +0200

    Prototyp programm

--- JS/Speisekarte.js
+++ JS/Speisekarte.js
@@ -1,4 +1,18 @@
+//const speisekarte = require("./menuinteraction.js")
+const Essenselemt={
+    id:undefined,
+    class:"anzeige_sp",
+    img:{
+        src: undefined,
+        alt: undefined
+    },
+    p:{
+        class: "speise_sp",
+        innerHTML: undefined
+    }
 
+};
+let table = undefined;
 /*
 TODO: 
 ELEMENT FÜR ESSEN definieren
@@ -9,3 +23,48 @@ mögliche redundanz ..> Speisekarte & Menu für editieren, vielleicht mit mehrer
 JS arbeiten und auf menu/speisekarte mit parametern unterscheiden
 GET für Speieskarte
 */
+document.addEventListener("DOMContentLoaded", ()=>{
+    table = document.getElementsByTagName("table");
+    loadfood();
+// alle gerichtarten
+//eventlistener hinzufügen; filterfood
+//eventlistener hinzufügen; resetfilter
+
+});
+
+async function getbeschreibung(){
+
+}
+
+async function loadfood(loadforedit=false){
+    let response = await fetch("/gericht/alle");
+    gerichtjson = await response.json();
+    console.log(gerichtjson)
+    counter = 1;
+    for(gericht of gerichtjson.body){
+        toappendgericht = Essenselemt;
+        toappendgericht.img.src=gerichtjson
+        toappendgericht.id=""
+        toappendgericht.p.innerHTML=""
+        tablerow.append(gerichtelement)
+    if(counter%4==0){
+        table.append("tr")
+
+    }
+
+    }
+    //get methode: alles
+    // für ergbniss element
+    // DOM element mit ergebnisswerten erstellen
+    // DOM element anhängen
+    //END for
+
+}
+
+function filterfood(){
+        //get methode: mit parametern
+    // für ergbniss element
+    // DOM element mit ergebnisswerten erstellen
+    // DOM element anhängen
+    //END for
+}
--- JS/hinzu.js
+++ JS/hinzu.js
@@ -1,10 +1,4 @@
-let InfoJSON ={
-    name: undefined,
-    beschreibung: undefined,
-    zutaten: undefined,
-    rezept: undefined,
-    bild: undefined
-}
+
 /*
 TODO:
 POST für hinzufügen
@@ -17,10 +11,26 @@ document.onload(initiate())
 function initiate(){
 let table = document.getElementById("tb");
 let submitbtn =document.getElementById("buttonsave");
+let namefield = document.getElementById("name");
+let beschreibung = document.getElementById("beschreibung");
+let zutatenliste = document.getElementById("zutaten");
+let rezept = document.getElementById("rezept");
+let bild= document.getElementById("bild");
 
 submitbtn.addEventListener("onclick", (clickevent) =>{
 clickevent.preventDefault();
-
+fetch('localhost:7000', {
+    method: 'POST',
+    body: {
+        bezeichnung: namefield.value,
+        zubereitung: rezept.value,
+        bildpfad:   bild.value,
+        zutatenliste: zutatenliste.value
+    },
+    headers:{
+        "content-type": "application/json"
+    }
+})
 });
 
     
